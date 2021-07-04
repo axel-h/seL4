82223f4ef1e1d398bc444e915923f2eab38cbebd
Axel Heider <axelheider@gmx.de>
2021-07-04 17:45:53 +0200

    boot: make insert_region() static
    The function insert_region() is not used outside of boot.c, so there
    is not reason to make it publicly available.
    
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- include/kernel/boot.h
+++ include/kernel/boot.h
@@ -51,7 +51,6 @@ bool_t init_freemem(word_t n_available, const p_region_t *available,
                     word_t n_reserved, const region_t *reserved,
                     v_region_t it_v_reg, word_t extra_bi_size_bits);
 bool_t reserve_region(p_region_t reg);
-bool_t insert_region(region_t reg);
 void write_slot(slot_ptr_t slot_ptr, cap_t cap);
 cap_t create_root_cnode(void);
 bool_t provide_cap(cap_t root_cnode_cap, cap_t cap);
