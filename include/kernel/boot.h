a7f358dbc99f40fc09a9e56dd8b496fa2ada22ae
Axel Heider <axelheider@gmx.de>
2021-09-02 06:58:40 +0200

    boot: remove macro BI_REF() and BI_PTR()
    BI_REF() is not used anywhere. BI_PTR() is only used in one place in the boot code.
    
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- include/kernel/boot.h
+++ include/kernel/boot.h
@@ -11,10 +11,6 @@
 #include <sel4/bootinfo_types.h>
 #include <arch/bootinfo.h>
 
-/* declare object-specific macros to hide the casting */
-#define BI_PTR(r) ((seL4_BootInfo*)(r))
-#define BI_REF(p) ((word_t)(p))
-
 #define S_REG_EMPTY (seL4_SlotRegion){ .start = 0, .end = 0 }
 
 /*
