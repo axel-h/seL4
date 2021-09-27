89a0ea93ec3f716931769fe992e1c9010611615f
Axel Heider <axelheider@gmx.de>
2021-09-27 14:46:15 +0200

    Merge compound_types.h into types.h
    The header file types.h is the only file that includes compound_types.h,
    so the content can be merged there. This simplifies the include
    structure a bit.
    
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- include/types.h
+++ include/types.h
@@ -7,4 +7,21 @@
 #pragma once
 
 #include <basic_types.h>
-#include <compound_types.h>
+#include <api/types.h>
+#include <object/structures.h>
+
+typedef struct pde_range {
+    pde_t *base;
+    word_t length;
+} pde_range_t;
+
+typedef struct pte_range {
+    pte_t *base;
+    word_t length;
+} pte_range_t;
+
+typedef cte_t *cte_ptr_t;
+
+typedef struct extra_caps {
+    cte_ptr_t excaprefs[seL4_MsgMaxExtraCaps];
+} extra_caps_t;
