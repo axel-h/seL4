a42ea54f0f0662ed58538b2e3662f08507706deb
Axel Heider <axelheider@gmx.de>
2021-12-02 12:24:11 +0100

    risc-v: provide wrapper for new SBI interface
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- include/arch/riscv/arch/sbi.h
+++ include/arch/riscv/arch/sbi.h
@@ -26,18 +26,18 @@
  * MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
  */
 
-/* This file is copied from RISC-V tools/linux project, it might change for
- * new spec releases.
- */
+/* This file was derived from RISC-V tools/linux project */
 
 #pragma once
 
 #include <config.h>
 #include <stdint.h>
 
-/* See https://github.com/riscv/riscv-sbi-doc/blob/master/riscv-sbi.adoc for
- * details about these command codes, they are the "legacy extensions"
- * introduced by BBL and supported by OpenSBI.
+/* The RISC-V Supervisor Binary Interface (SBI) specification is available at
+ * https://github.com/riscv-non-isa/riscv-sbi-doc and covers both the legacy
+ * interface and the new extensible interface. We have to support platform that
+ * just implement the legacy interface and it is sufficient what the kernel
+ * needs, thus the SBI wrappers below keep using it.
  */
 #define SBI_SET_TIMER 0
 #define SBI_CONSOLE_PUTCHAR 1
@@ -50,57 +50,91 @@
 #define SBI_SHUTDOWN 8
 /* The values 9 - 15 are reserved. */
 
-static inline word_t sbi_call(word_t cmd,
-                              word_t arg_0,
-                              word_t arg_1,
-                              word_t arg_2)
+typedef struct {
+    word_t error;
+    word_t value;
+} sbiret_t;
+
+static inline sbiret_t sbi_call(word_t extension_id,
+                                word_t function_id,
+                                word_t arg_0,
+                                word_t arg_1,
+                                word_t arg_2,
+                                word_t arg_3)
 {
     register word_t a0 asm("a0") = arg_0;
     register word_t a1 asm("a1") = arg_1;
     register word_t a2 asm("a2") = arg_2;
-    register word_t a7 asm("a7") = cmd;
-    register word_t result asm("a0");
+    register word_t a3 asm("a3") = arg_3;
+    register word_t a6 asm("a6") = function_id;
+    register word_t a7 asm("a7") = extension_id;
+
+    /* Seems the proofs don't support "+r" for inline assembly to declare that
+     * a variable (actually a register) is modified. Only "=r" is supported, so
+     * there are strict separation between input and output variables.
+     */
+    register word_t a0_ret asm("a0");
+    register word_t a1_ret asm("a1");
+
     asm volatile("ecall"
-                 : "=r"(result)
-                 : "r"(a0), "r"(a1), "r"(a2), "r"(a7)
+                 : "=r"(a0_ret), "=r"(a1_ret)
+                 : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a6), "r"(a7)
                  : "memory");
-    return result;
+
+    return (sbiret_t) {
+        .error = a0_ret,
+        .value = a1_ret
+    };
 }
 
-/* Lazy implementations until SBI is finalized */
-#define SBI_CALL_0(which) sbi_call(which, 0, 0, 0)
-#define SBI_CALL_1(which, arg0) sbi_call(which, arg0, 0, 0)
-#define SBI_CALL_2(which, arg0, arg1) sbi_call(which, arg0, arg1, 0)
+static inline word_t sbi_call_legacy(word_t cmd,
+                                     word_t arg_0,
+                                     word_t arg_1,
+                                     word_t arg_2,
+                                     word_t arg_3)
+{
+    /* The new interface was designed as an extension of the legacy interface,
+     * the command became the extension ID and the function ID was added as a
+     * new parameter, which legacy calls ignore.
+     */
+    sbiret_t sbiret = sbi_call(cmd, 0, arg_0, arg_1, arg_2, arg_3);
+    return sbiret.error;
+}
+
 
 static inline void sbi_console_putchar(int ch)
 {
-    SBI_CALL_1(SBI_CONSOLE_PUTCHAR, ch);
+    (void)sbi_call_legacy(SBI_CONSOLE_PUTCHAR, ch, 0, 0, 0);
 }
 
 static inline int sbi_console_getchar(void)
 {
-    return (int)(SBI_CALL_0(SBI_CONSOLE_GETCHAR));
+    return (int)sbi_call_legacy(SBI_CONSOLE_GETCHAR, 0, 0, 0, 0);
 }
 
-static inline void sbi_set_timer(unsigned long long stime_value)
+static inline void sbi_set_timer(uint64_t timestamp)
 {
-#if __riscv_xlen == 32
-    SBI_CALL_2(SBI_SET_TIMER, stime_value, stime_value >> 32);
+#if defined(CONFIG_ARCH_RISCV32)
+    word_t timestamp_hi = (word_t)timestamp;
+    word_t timestamp_lo = (word_t)(timestamp >> 32);
+    (void)sbi_call_legacy(SBI_SET_TIMER, timestamp_hi, timestamp_lo, 0, 0);
+#elif defined(CONFIG_ARCH_RISCV64)
+    (void)sbi_call_legacy(SBI_SET_TIMER, timestamp, 0, 0, 0);
 #else
-    SBI_CALL_1(SBI_SET_TIMER, stime_value);
+#error "unsupported architecture"
 #endif
 }
 
 static inline void sbi_shutdown(void)
 {
-    SBI_CALL_0(SBI_SHUTDOWN);
+    (void)sbi_call_legacy(SBI_SHUTDOWN, 0, 0, 0, 0);
 }
 
 #ifdef ENABLE_SMP_SUPPORT
 
 static inline void sbi_clear_ipi(void)
 {
-    SBI_CALL_0(SBI_CLEAR_IPI);
+    (void)sbi_call_legacy(SBI_CLEAR_IPI, 0, 0, 0, 0);
 }
 
 static inline void sbi_send_ipi(word_t hart_mask)
@@ -125,33 +159,35 @@ static inline void sbi_send_ipi(word_t hart_mask)
      *           solution and higher layers are kept agnostic of this.
      */
     word_t virt_addr_hart_mask = (word_t)&hart_mask;
-    SBI_CALL_1(SBI_SEND_IPI, virt_addr_hart_mask);
+    (void)sbi_call_legacy(SBI_SEND_IPI, virt_addr_hart_mask, 0, 0, 0);
 }
 
 static inline void sbi_remote_fence_i(word_t hart_mask)
 {
     /* See comment at sbi_send_ipi() about the pointer parameter. */
     word_t virt_addr_hart_mask = (word_t)&hart_mask;
-    SBI_CALL_1(SBI_REMOTE_FENCE_I, virt_addr_hart_mask);
+    (void)sbi_call_legacy(SBI_REMOTE_FENCE_I, virt_addr_hart_mask, 0, 0, 0);
 }
 
 static inline void sbi_remote_sfence_vma(word_t hart_mask,
-                                         unsigned long start,
-                                         unsigned long size)
+                                         word_t start,
+                                         word_t size)
 {
     /* See comment at sbi_send_ipi() about the pointer parameter. */
     word_t virt_addr_hart_mask = (word_t)&hart_mask;
-    SBI_CALL_1(SBI_REMOTE_SFENCE_VMA, virt_addr_hart_mask);
+    (void)sbi_call_legacy(SBI_REMOTE_SFENCE_VMA, virt_addr_hart_mask, start,
+                          size, 0);
 }
 
 static inline void sbi_remote_sfence_vma_asid(word_t hart_mask,
-                                              unsigned long start,
-                                              unsigned long size,
-                                              unsigned long asid)
+                                              word_t start,
+                                              word_t size,
+                                              word_t asid)
 {
     /* See comment at sbi_send_ipi() about the pointer parameter. */
     word_t virt_addr_hart_mask = (word_t)&hart_mask;
-    SBI_CALL_1(SBI_REMOTE_SFENCE_VMA_ASID, virt_addr_hart_mask);
+    (void)sbi_call_legacy(SBI_REMOTE_SFENCE_VMA_ASID, virt_addr_hart_mask,
+                          start, size, asid);
 }
 
 #endif /* ENABLE_SMP_SUPPORT */
