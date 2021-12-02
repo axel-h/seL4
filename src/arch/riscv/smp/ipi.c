b8af7a0e3b7f62af9c8b19ee0b75be845bcf2068
Axel Heider <axelheider@gmx.de>
2021-12-02 12:05:39 +0100

    risc-v: cleanup includes and guard
    - prototype for ipi_send_target() is in include/smp/ipi.h
    - add comment what conditional block ended
    - avoid unnecessary includes
    
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- src/arch/riscv/smp/ipi.c
+++ src/arch/riscv/smp/ipi.c
@@ -5,11 +5,12 @@
  */
 
 #include <config.h>
+
+#ifdef ENABLE_SMP_SUPPORT
+
 #include <mode/smp/ipi.h>
 #include <smp/lock.h>
 #include <util.h>
-
-#ifdef ENABLE_SMP_SUPPORT
 
 /* the remote call being requested */
 static volatile IpiRemoteCall_t  remoteCall;
@@ -88,4 +89,4 @@ void ipi_send_target(irq_t irq, word_t hart_id)
     sbi_send_ipi(hart_mask);
 }
 
-#endif
+#endif /* ENABLE_SMP_SUPPORT */
