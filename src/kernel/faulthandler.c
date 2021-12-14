17f856ab684b6972be982fc26080da19779bc74e
Axel Heider <axelheider@gmx.de>
2021-12-14 06:25:02 +0100

    debug: improve print_fault_handler()
    - Re-word the fault message so it easier to understand
    - Print the register state
    
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- src/kernel/faulthandler.c
+++ src/kernel/faulthandler.c
@@ -13,59 +13,97 @@
 
 #ifdef CONFIG_PRINTING
 
+#if CONFIG_WORD_SIZE == 32
+#define PRI_reg "%08"SEL4_PRIx_word
+#define PRI_reg_param(r)    (r)
+#elif CONFIG_WORD_SIZE == 64
+#define PRI_reg "%08"PRIx32"'%08"PRIx32
+#define PRI_reg_param(r)    (uint32_t)((r) >> 32), (uint32_t)(r)
+#else
+#error "unsupported CONFIG_WORD_SIZE"
+#endif
+
 static void print_fault(seL4_Fault_t f)
 {
-    switch (seL4_Fault_get_seL4_FaultType(f)) {
+    const char *name = config_ternary(CONFIG_DEBUG_BUILD,
+                                      TCB_PTR_DEBUG_PTR(tptr)->tcbName,
+                                      null);
+
+    printf("\n\n"
+           "####\n"
+           "####  FAULT at PC=%p in thread %p%s%s%s\n"
+           "####\n",
+           (void *)getRestartPC(tptr), tptr,
+           name ? " (" : "", name, name ? ")" : "");
+
+
+    seL4_Fault_t fault = tptr->tcbFault;
+    word_t fault_type = seL4_Fault_get_seL4_FaultType(fault);
+    printf("Cause: "); /* message follows here */
+    switch (fault_type) {
     case seL4_Fault_NullFault:
         printf("null fault");
         break;
     case seL4_Fault_CapFault:
+        /* tptr->tcbLookupFailure is also set */
         printf("cap fault in %s phase at address %p",
-               seL4_Fault_CapFault_get_inReceivePhase(f) ? "receive" : "send",
-               (void *)seL4_Fault_CapFault_get_address(f));
+               seL4_Fault_CapFault_get_inReceivePhase(fault) ? "receive" : "send",
+               (void *)seL4_Fault_CapFault_get_address(fault));
         break;
     case seL4_Fault_VMFault:
         printf("vm fault on %s at address %p with status %p",
-               seL4_Fault_VMFault_get_instructionFault(f) ? "code" : "data",
-               (void *)seL4_Fault_VMFault_get_address(f),
-               (void *)seL4_Fault_VMFault_get_FSR(f));
+               seL4_Fault_VMFault_get_instructionFault(fault) ? "code" : "data",
+               (void *)seL4_Fault_VMFault_get_address(fault),
+               (void *)seL4_Fault_VMFault_get_FSR(fault));
         break;
     case seL4_Fault_UnknownSyscall:
         printf("unknown syscall %p",
-               (void *)seL4_Fault_UnknownSyscall_get_syscallNumber(f));
+               (void *)seL4_Fault_UnknownSyscall_get_syscallNumber(fault));
         break;
     case seL4_Fault_UserException:
         printf("user exception %p code %p",
-               (void *)seL4_Fault_UserException_get_number(f),
-               (void *)seL4_Fault_UserException_get_code(f));
+               (void *)seL4_Fault_UserException_get_number(fault),
+               (void *)seL4_Fault_UserException_get_code(fault));
         break;
 #ifdef CONFIG_KERNEL_MCS
     case seL4_Fault_Timeout:
-        printf("Timeout fault for 0x%x\n", (unsigned int) seL4_Fault_Timeout_get_badge(f));
+        printf("Timeout fault for 0x%x", (unsigned int)seL4_Fault_Timeout_get_badge(fault));
         break;
 #endif
     default:
-        printf("unknown fault");
+        printf("unknown fault 0x%x", (unsigned int)fault_type);
         break;
     }
-}
 
-static void printFaultHandlerError(tcb_t *tptr, seL4_Fault_t fault)
-{
-#ifdef CONFIG_KERNEL_MCS
-    printf("Found thread has no fault handler while trying to handle:\n");
-#else // not CONFIG_KERNEL_MCS
-    printf("Caught ");
-    print_fault(current_fault);
-    printf("\nwhile trying to handle:\n");
-#endif
+    printf("\n");
+
+    /* Thread registers are printed in debug builds only. */
 #ifdef CONFIG_DEBUG_BUILD
-    print_fault(fault);
-    printf("\nin thread %p \"%s\" ", tptr, TCB_PTR_DEBUG_PTR(tptr)->tcbName);
-#endif
-    printf("at address %p\n", (void *)getRestartPC(tptr));
-    printf("With stack:\n");
+    printf("Registers:\n");
+    user_context_t *user_ctx = &(tptr->tcbArch.tcbContext);
+    /* Dynamically adapt spaces to max register name len per column. */
+    unsigned int max_reg_name_len[2] = {0};
+    for (unsigned int i = 0; i < ARRAY_SIZE(user_ctx->registers); i++) {
+        int col = i & 1;
+        unsigned int l = strnlen(register_names[i], 20);
+        if (max_reg_name_len[col] < l) {
+            max_reg_name_len[col] = l;
+        }
+    }
+    const unsigned int num_regs = ARRAY_SIZE(user_ctx->registers);
+    for (unsigned int i = 0; i < num_regs; i++) {
+        int col = i & 1;
+        bool_t is_last = (num_regs == i + 1);
+        word_t reg = user_ctx->registers[i];
+        printf("%*s: 0x"PRI_reg"%s",
+               max_reg_name_len[col] + 2, register_names[i], PRI_reg_param(reg),
+               (col || is_last) ? "\n" : "");
+    }
+    printf("\nStack:\n");
     Arch_userStackTrace(tptr);
+#endif /* CONFIG_DEBUG_BUILD */
+
+    printf("\nThread suspended, no userland fault handler\n");
 }
 
 #endif /* CONFIG_PRINTING */
