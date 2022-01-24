86c9aa32cdf4bbc12f3ac6b582c8531cfedef064
Axel Heider <axelheider@gmx.de>
2022-01-24 17:04:31 +0100

    use MCS_TERNARY to drop macro MCS_DO_IF_BUDGET()
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- src/api/syscall.c
+++ src/api/syscall.c
@@ -200,7 +200,8 @@ exception_t handleUnknownSyscall(word_t w)
     } /* end switch(w) */
 #endif /* CONFIG_ENABLE_BENCHMARKS */
 
-    MCS_DO_IF_BUDGET({
+    if (likely(MCS_TERNARY(checkBudgetWithRestart(), true))) {
+
 #ifdef CONFIG_SET_TLS_BASE_SELF
         if (w == SysSetTLSBase)
         {
@@ -215,7 +216,8 @@ exception_t handleUnknownSyscall(word_t w)
 #endif
         current_fault = seL4_Fault_UnknownSyscall_new(w);
         handleFault(NODE_STATE(ksCurThread));
-    })
+
+    }
 
     schedule();
     activateThread();
@@ -225,10 +227,11 @@ exception_t handleUnknownSyscall(word_t w)
 
 exception_t handleUserLevelFault(word_t w_a, word_t w_b)
 {
-    MCS_DO_IF_BUDGET({
+    if (likely(MCS_TERNARY(checkBudgetWithRestart(), true))) {
         current_fault = seL4_Fault_UserException_new(w_a, w_b);
         handleFault(NODE_STATE(ksCurThread));
-    })
+    }
+
     schedule();
     activateThread();
 
@@ -237,14 +240,13 @@ exception_t handleUserLevelFault(word_t w_a, word_t w_b)
 
 exception_t handleVMFaultEvent(vm_fault_type_t vm_faultType)
 {
-    MCS_DO_IF_BUDGET({
-
+    if (likely(MCS_TERNARY(checkBudgetWithRestart(), true))) {
         exception_t status = handleVMFault(NODE_STATE(ksCurThread), vm_faultType);
         if (status != EXCEPTION_NONE)
         {
             handleFault(NODE_STATE(ksCurThread));
         }
-    })
+    }
 
     schedule();
     activateThread();
@@ -511,8 +513,9 @@ exception_t handleSyscall(syscall_t syscall)
 {
     exception_t ret;
     irq_t irq;
-    MCS_DO_IF_BUDGET({
-        switch (syscall)
+
+    if (likely(MCS_TERNARY(checkBudgetWithRestart(), true))) {
+        switch (syscall) 
         {
         case SysSend:
             ret = handleInvocation(false, true, false, false, getRegister(NODE_STATE(ksCurThread), capRegister));
@@ -618,7 +621,7 @@ exception_t handleSyscall(syscall_t syscall)
             fail("Invalid syscall");
         }
 
-    })
+    }
 
     schedule();
     activateThread();
