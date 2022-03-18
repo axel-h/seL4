1196220c01dadc6168a9a30ae6b0a36d574cae46
Axel Heider <axelheider@gmx.de>
2022-03-18 19:14:30 +0100

    support board "acme42" of architecture ACME64

---
+++ libsel4/sel4_plat_include/acme42/sel4/plat/api/constants.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2022, Axel Heider <axelheider@gmx.de>
+ * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <autoconf.h>
+
+/* nothing here */
