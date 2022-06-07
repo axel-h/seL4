06f20b072c920f20447beef3b43c75f280061711
Axel Heider <axelheider@gmx.de>
2022-06-07 14:56:32 +0200

    add CONFIG_BKL_SWAP_MANUAL
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- include/arch/arm/arch/model/smp.h
+++ include/arch/arm/arch/model/smp.h
@@ -16,47 +16,52 @@ static inline cpu_id_t cpuIndexToID(word_t index)
     return BIT(index);
 }
 
-static inline bool_t try_arch_atomic_exchange_rlx(void *ptr, void *new_val, void **prev)
+#ifdef CONFIG_BKL_SWAP_MANUAL
+static inline void *arch_atomic_exchange(void *head, void *node)
 {
-    /* The ARM architecture had an atomic swap instruction in ARMv6, which got
-     * deprecated and war replaced by exclusive load and store instructions in
-     * ARMv7. The compiler builtin __atomic_exchange_n() executed them in a loop
-     * as long as the exclusivity fail fails. Unfortunately, this approach can
-     * results in an undefined delay up to seconds in the worst case if other
-     * cores touch memory in the same exclusivity reservation granule. The
-     * actual granule size is implementation specific. The explicit load and
-     * store instruction below and reporting back success or failure is the only
-     * way to work around this, the caller can loop over calling this functions
-     * this and perform other maintenance operation or wait on failure.
+    /* ARMv6 had an atomic swap instruction. ARMv7 deprecated is and recommended
+     * using exclusive load and store instructions, which the compiler builtin
+     * __atomic_exchange_n() executes in a loop as long as the exclusivity fail
+     * fails. Unfortunately, this approach can results in an undefined delay up
+     * to seconds in the worst case if other cores touch memory in the same
+     * exclusivity reservation granule. The actual granule size is
+     * implementation specific, for modern SMP optimized cores the size is
+     * expected to be small.
      * ARM v8.1 eventually brought the atomic swap instruction back as part of
      * LSE (Large System Extensions) and GCC support was added in v9.4. Thus,
      * using __atomic_exchange_n() becomes an option when "-march=armv8.1-a" is
      * passed on platform that use core implementing ARMv8.1 or higher. Details
      * remain to be analyzed.
      */
-    uint32_t atomic_status;
-    void *temp;
+    __atomic_thread_fence(__ATOMIC_RELEASE); /* all writes must finish */
 
-    asm volatile(
-        LD_EX "%[prev_output], [%[ptr_val]]             \n\t" /* ret = *ptr */
-        ST_EX "%" OP_WIDTH "[atomic_var], %[new_val] , [%[ptr_val]] \n\t"  /* *ptr = new */
-        : [atomic_var] "=&r"(atomic_status), [prev_output]"=&r"(temp)     /* output */
-        : [ptr_val] "r"(ptr), [new_val] "r"(new_val)   /* input */
-        :
-    );
+    for (;;)  {
+        void *prev;
+        uint32_t atomic_status;
+        asm volatile(
+            LD_EX "%[prev_output], [%[ptr_val]]                         \n\t" /* prev = *head */
+            ST_EX "%" OP_WIDTH "[atomic_var], %[new_val] , [%[ptr_val]] \n\t" /* *head = node */
+            : /* output */ [atomic_var] "=&r"(atomic_status), [prev_output]"=&r"(prev)
+            : /* input */ [ptr_val] "r"(head), [new_val] "r"(node)
+            : /* no clobber */
+        );
 
-    if (0 != atomic_status) {
-        /* Specs say 0 indicates success and 1 a exclusivity failure, any other
-         * value is not defined. If the atomic operation has failed, prev is
-         * left untouched. There seems no gain updating it with the value
-         * obtained, as this might no longer be valid anyway.
+        /* Specs say 0 indicates success and 1 an exclusivity failure, any other
+         * value is not defined.
          */
-        return false;
+        if (0 == atomic_status) {
+            /* The write for the update of the queue write has finished anyway,
+             * prevent pipeline from starting any reads before passing here
+             */
+            __atomic_thread_fence(__ATOMIC_ACQUIRE);
+            return prev;
+        }
+
+        /* keep spinning */
     }
 
-    *prev = temp;
-    return true;
 }
+#endif /* CONFIG_BKL_SWAP_MANUAL */
 
 #endif /* ENABLE_SMP_SUPPORT */
 
