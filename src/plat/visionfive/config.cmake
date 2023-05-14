e022e27a6342524a036d55d304ead3cd21867f73
Axel Heider <axelheider@gmx.de>
2023-05-15 01:39:57 +0200

    wip doc

--- src/plat/visionfive/config.cmake
+++ src/plat/visionfive/config.cmake
@@ -9,6 +9,19 @@ cmake_minimum_required(VERSION 3.7.2)
 declare_platform(visionfive KernelPlatformVisionFive PLAT_VISIONFIVE KernelArchRiscV)
 
 if(KernelPlatformVisionFive)
+
+    # Notes:
+    #   VifionFive v1: JH7100
+    #   VifionFive v2: JH71110
+    #
+    # JH7100 Issues:
+    #   - see https://github.com/starfive-tech/JH7100_Docs
+    #   - problem with cacheability and coherence of I/O registers and buffers.
+    #     - See https://www.reddit.com/r/RISCV/comments/13h12yv/visionfive_2_l2_cache_integrity/
+    #     - Affected peripherals:  SDIO, GMAC, USB 3.0
+    #     - must flush L2 cache to DRAM when DMA is used
+
+
     declare_seL4_arch(riscv64)
     config_set(KernelRiscVPlatform RISCV_PLAT "visionvive")
     config_set(KernelPlatformFirstHartID FIRST_HART_ID 1)
