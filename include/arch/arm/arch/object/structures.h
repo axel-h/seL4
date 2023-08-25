07a38bfd2e73d53cba98f7c725891e8c5d4364d0
Axel Heider <axelheider@gmx.de>
2023-08-25 15:52:57 +0200

    use switch/case
    TODO: verification does not like a switch/case with
    just a default statement....

--- include/arch/arm/arch/object/structures.h
+++ include/arch/arm/arch/object/structures.h
@@ -12,17 +12,13 @@
 
 static inline bool_t CONST Arch_isCapRevocable(cap_t derivedCap, cap_t srcCap)
 {
-#ifdef CONFIG_ALLOW_SMC_CALLS
     switch (cap_get_capType(derivedCap)) {
+#ifdef CONFIG_ALLOW_SMC_CALLS
     case cap_smc_cap:
         return (cap_smc_cap_get_capSMCBadge(derivedCap) !=
                 cap_smc_cap_get_capSMCBadge(srcCap));
-
+#endif
     default:
         return false;
     }
-#endif
-    return false;
 }
-
-
