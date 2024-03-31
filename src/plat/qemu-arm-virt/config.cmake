74f1a5e7ac8b435b9f2d4ffc4a2ba6dcdc328aa5
Axel Heider <axel.heider@codasip.com>
2024-03-31 03:27:39 +0200

    cmake: fix typos
    Signed-off-by: Axel Heider <axel.heider@codasip.com>

--- src/plat/qemu-arm-virt/config.cmake
+++ src/plat/qemu-arm-virt/config.cmake
@@ -170,7 +170,7 @@ if(KernelPlatformQEMUArmVirt)
             # it can be added to the DTS as reference. Every parameter must be
             # a separate string, as CMake will make it a dedicated argument
             # passed to QEMU then (e.g. "-machine virt" wont be recognized,
-            # but "-machine", "vir" is).
+            # but "-machine", "virt" is).
             set(
                 QEMU_CMD
                 "${QEMU_BINARY}"
