5a864c4f424c992332c6a0e1f5c6fb2a4f94cad2
Axel Heider <axelheider@gmx.de>
2024-07-12 20:34:15 +0200

    add isValidFaultHandlerEp()
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- include/kernel/faulthandler.h
+++ include/kernel/faulthandler.h
@@ -8,10 +8,25 @@
 
 #include <object.h>
 
+static bool_t isValidFaultHandlerEp(cap_t cap)
+{
+    return ((cap_get_capType(cap) == cap_endpoint_cap) &&
+            cap_endpoint_cap_get_capCanSend(cap) &&
+            (cap_endpoint_cap_get_capCanGrant(cap) ||
+             cap_endpoint_cap_get_capCanGrantReply(cap)));
+}
+
 #ifdef CONFIG_KERNEL_MCS
+
+static bool_t isValidFaultHandlerEpOrNull(cap_t cap)
+{
+    return ((cap_get_capType(cap) == cap_null_cap) || isValidFaultHandlerEp(cap));
+}
+
 static inline bool_t validTimeoutHandler(tcb_t *tptr)
 {
-    return cap_get_capType(TCB_PTR_CTE_PTR(tptr, tcbTimeoutHandler)->cap) == cap_endpoint_cap;
+    cap_t handlerCap = TCB_PTR_CTE_PTR(tptr, tcbTimeoutHandler)->cap;
+    return isValidFaultHandlerEp(handlerCap);
 }
 
 void handleTimeout(tcb_t *tptr);
